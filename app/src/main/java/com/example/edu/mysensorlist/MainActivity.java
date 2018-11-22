01b34be0f457715aefd5efa3b330f220d4e0f30f
lesnyg <lsy79@hanmail.net>
2018-11-22 11:07:10 +0900

    commit1

--- app/src/main/java/com/example/edu/mysensorlist/MainActivity.java
+++ app/src/main/java/com/example/edu/mysensorlist/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.edu.mysensorlist;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textViewSensorList = findViewById(R.id.textView);
+
+        StringBuffer report = new StringBuffer();
+        SensorManager sensorManager = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
+        List<Sensor> sensorList = sensorManager.getSensorList(Sensor.TYPE_ALL);
+        report.append("All Sensor : ").append(sensorList.size()).append("\n");
+        int i = 0;
+        for(Sensor sensor:sensorList){
+            report.append(i++ +"Name : "+sensor.getName()+"\n")
+                    .append("\t Power : "+sensor.getPower()+"\n")
+                    .append("\t Resolution : "+sensor.getResolution()+"\n")
+                    .append("\t Range : "+sensor.getMaximumRange()+"\n");
+        }
+        textViewSensorList.setText(report.toString());
     }
 }
